e30248739714c902630b1b87e7121157a1b95307
yun2h <dbsgmldl02@naver.com>
2025-05-13 23:48:48 +0900

    #11 fix: 팝업 main 디자인, 빌드 오류 수정

--- front/chrome-extension/vite.config.js
+++ front/chrome-extension/vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        drag: resolve(__dirname, 'public/content.jsx'), // 여기!
-      },
-      output: {
-        entryFileNames: '[name].js',
-      },
-    },
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-});
+})
