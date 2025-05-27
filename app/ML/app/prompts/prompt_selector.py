0ceaf2f54c5421710c0b51c9e7f0dabe23ff5b85
youcy8585 <138505714+youcy8585@users.noreply.github.com>
2025-05-27 21:46:34 +0900

    feat : 불공정조항 매핑 수정

--- app/ML/app/prompts/prompt_selector.py
+++ app/ML/app/prompts/prompt_selector.py
@@ -30,28 +30,28 @@ def get_summary_detect(category: str):
     return PROMPT_SUM_MAP.get(category)
 
 PROMPT_DET_MAP = {
-    "processingPurpose": unfair_detect_purpose_template,
-    "collectedItems": unfair_detect_items_template,
-    "childrenUnder14": unfair_detect_children_under_14_template, # 추가
-    "retentionPeriod": unfair_detect_retention_period_template,
-    "destructionProcedure": unfair_detect_destruction_template,
+    "processing_info": unfair_detect_processing_info_template,
+    #"collectedItems": unfair_detect_items_template,
+    #"childrenUnder14": unfair_detect_children_under_14_template, # 추가
+    #"retentionPeriod": unfair_detect_retention_period_template,
+    "storage_deletion": unfair_detect_storage_deletion_template,
     "thirdPartySharing": unfair_detect_third_party_template,
-    "additionalUseCriteria": unfair_detect_additional_use_template,
+    #"additionalUseCriteria": unfair_detect_additional_use_template,
     "outsourcingInfo": unfair_detect_outsourcing_template,
     "overseasTransfer": unfair_detect_overseas_transfer_template,
     "securityMeasures": unfair_detect_security_measures_template,
-    "sensitiveInfoDisclosure": unfair_detect_sensitive_info_template,
-    "pseudonymizedInfo": unfair_detect_pseudonymized_info_template, # 추가
+    #"sensitiveInfoDisclosure": unfair_detect_sensitive_info_template,
+    #"pseudonymizedInfo": unfair_detect_pseudonymized_info_template, # 추가
     "autoCollectionDevices": unfair_detect_auto_collection_template,
     "behavioralTrackingByThirdParties": unfair_detect_behavior_tracking_template,
     "dataSubjectRights": unfair_detect_rights_and_methods_template,
-    "privacyOfficerInfo": unfair_detect_privacy_officer_template,
-    "domesticAgent": unfair_detect_domestic_agent_template, # 추가
-    "remedyForInfringement": unfair_detect_remedy_template, # 추가
-    "fixedCCTVOperation": unfair_detect_fixed_cctv_template, # 추가
-    "mobileCCTVOperation": unfair_detect_mobile_cctv_template, # 추가
-    "optionalPrivacyClauses": unfair_detect_optional_clauses_template, # 추가
-    "policyChanges": unfair_detect_policy_changes_template,
+    #"privacyOfficerInfo": unfair_detect_privacy_officer_template,
+    #"domesticAgent": unfair_detect_domestic_agent_template, # 추가
+    "user_protection": unfair_detect_user_protection_template, # 추가
+    #"fixedCCTVOperation": unfair_detect_fixed_cctv_template, # 추가
+    #"mobileCCTVOperation": unfair_detect_mobile_cctv_template, # 추가
+    #"optionalPrivacyClauses": unfair_detect_optional_clauses_template, # 추가
+    #"policyChanges": unfair_detect_policy_changes_template,
 }
 
 def get_detect_prompt(category: str):
